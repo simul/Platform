38f4751a32897040c0a058c4cb2a39d2cc93b33c
simul <simul@281afad1-045c-402e-9234-ccd8513c57c6>
2011-04-28 22:59:49 +0000

    git-svn-id: https://simul.svn.cvsdude.com/svn@885 281afad1-045c-402e-9234-ccd8513c57c6

--- Platform/Windows/DirectX 1x/SaveTextureDX1x.h
+++
@@ -1,6 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-// Direct3D includes
-#include <d3d10.h>
-
-extern void Screenshot(IDirect3DDevice9* pd3dDevice,const char *txt);
-extern void SaveTexture(LPDIRECT3DTEXTURE9 ldr_buffer_texture,const char *txt,bool as_dds);
