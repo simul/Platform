88a37e5e3543afc3e3dd6a73ebed6e92957bef40
Roderick Kennedy <roderick.kennedy@gmail.com>
2014-05-19 10:53:18 +0100

    .

--- Platform/DirectX11/GpuSkyGenerator.cpp
+++ Platform/DirectX11/GpuSkyGenerator.cpp
@@ -137,6 +137,7 @@ void GpuSkyGenerator::MakeLossAndInscatterTextures(
 		gpuSkyConstants.hazeScaleHeightKm	=p.physical.hazeStruct.haze_scale_height_km;
 
 		gpuSkyConstants.rayleigh			=(const float*)skyInterface->GetRayleigh();
+
 		gpuSkyConstants.hazeMie				=(const float*)(p.physical.hazeStruct.haze*p.physical.hazeStruct.mie);
 		gpuSkyConstants.ozone				=(const float*)(p.physical.ozone);
 
@@ -146,7 +147,7 @@ void GpuSkyGenerator::MakeLossAndInscatterTextures(
 		gpuSkyConstants.starlight			=(const float*)(p.physical.starlight);
 		
 		gpuSkyConstants.hazeEccentricity	=1.0;
-		gpuSkyConstants.mieRayleighRatio	=(const float*)(skyInterface->GetMieRayleighRatio());
+		gpuSkyConstants.mieRayleighRatio	=(const float*)(GetMieRayleighRatio(p.physical.hazeStruct.mie,skyInterface->GetRayleigh()));
 		gpuSkyConstants.emissivity			=ir.emissivity;
 		gpuSkyConstants.yRange				=vec2(0.f,1.f);
 
