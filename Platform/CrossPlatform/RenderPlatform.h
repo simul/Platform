ee27df8c2a9afc612344fa71b7d97ecc85ba4139
Roderick Kennedy <roderick.kennedy@gmail.com>
2016-02-17 17:55:46 +0000

    Small API fixes

--- Platform/CrossPlatform/RenderPlatform.h
+++ Platform/CrossPlatform/RenderPlatform.h
@@ -164,9 +164,9 @@ namespace simul
 			/// Create a platform-specific light instance.
 			virtual Light					*CreateLight					()	=0;
 			/// Create a platform-specific texture instance.
-			virtual Texture					*CreateTexture					(const char *lFileNameUtf8=NULL)	=0;
+			virtual Texture					*CreateTexture					(const char *lFileNameUtf8 =nullptr)	=0;
 			/// Create a platform-specific framebuffer instance - i.e. an optional colour and an optional depth rendertarget. Optionally takes a name string.
-			virtual BaseFramebuffer			*CreateFramebuffer				(const char *)	=0;
+			virtual BaseFramebuffer			*CreateFramebuffer				(const char * =nullptr)	=0;
 			/// Create a platform-specific sampler state instance.
 			virtual SamplerState			*CreateSamplerState				(SamplerStateDesc *)	=0;
 			/// Look for a sampler state of the stated name, and create one if it does not exist. The resulting state will be owned by the RenderPlatform, so do not destroy it.
