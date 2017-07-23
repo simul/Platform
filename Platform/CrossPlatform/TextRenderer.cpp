d5fd09e8c1de551d72a15384cb5b8e61bdcb26c4
Roderick Kennedy <roderick@simul.co.uk>
2017-07-23 17:26:06 +0100

    Revert coverage change in simul_gpu_clouds.sfx

--- Platform/CrossPlatform/TextRenderer.cpp
+++ Platform/CrossPlatform/TextRenderer.cpp
@@ -171,6 +171,7 @@ void TextRenderer::Render(crossplatform::DeviceContext &deviceContext,float x,fl
 		clr=white;
 	if(!bck)
 		bck=transp;
+	static float fontScale=3.0;
 	//renderPlatform->DrawQuad(deviceContext,20,20,50,50,effect,effect->GetTechniqueByName("textured"),"noblend");
 	//renderPlatform->StoreRenderState(deviceContext);
 	constantBuffer.colour		=vec4(clr);
@@ -185,9 +186,9 @@ void TextRenderer::Render(crossplatform::DeviceContext &deviceContext,float x,fl
 		if(idx<0||idx>100)
 			continue;
 		const FontIndex &f=fontIndices[idx];
-		w+=f.pixel_width+1;
+		w+=f.pixel_width*fontScale+1;
 	}
-	static float ht=32.0f;
+	float ht=fontScale*20.0f;
 	//renderPlatform->SetStandardRenderState(deviceContext,crossplatform::STANDARD_ALPHA_BLENDING);
 	constantBuffer.text_rect		=vec4(2.0f*x/screen_width-1.f,1.f-2.0f*(y+ht)/screen_height,2.0f*(float)w/screen_width,2.0f*ht/screen_height);
 	if(mirrorY)
@@ -217,13 +218,13 @@ void TextRenderer::Render(crossplatform::DeviceContext &deviceContext,float x,fl
 		if(idx>0)
 		{
 			constantBuffer.text_rect.x	=2.0f*x/screen_width-1.f;
-			constantBuffer.text_rect.z	=2.0f*(float)f.pixel_width*2.0f/screen_width;
+			constantBuffer.text_rect.z	=2.0f*(float)f.pixel_width*fontScale/screen_width;
 			static float u			=1024.f/598.f;
 			constantBuffer.texc		=vec4(f.x*u,0.0f,(f.w-f.x)*u,1.0f);
 			effect->SetConstantBuffer(deviceContext,&constantBuffer);
 			renderPlatform->DrawQuad(deviceContext);
 		}
-		x+=f.pixel_width*2.0f+1;
+		x+=f.pixel_width*fontScale+1;
 	}
 	effect->UnbindTextures(deviceContext);
 	effect->Unapply(deviceContext);
