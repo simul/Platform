c7ac12554304df28b107c5af4b84e8827f7197e3
Roderick Kennedy <noreply@simul.co>
2019-02-01 12:32:22 +0000

    Cmake build fixes for static projects.

--- Platform/DirectX12/DirectX12.cmake
+++ Platform/DirectX12/DirectX12.cmake
@@ -10,7 +10,7 @@ file(GLOB SOURCES
 file(GLOB HEADERS
     "*.h"
 )
-add_library(SimulDirectX12_MT ${SOURCES} ${HEADERS} ${CMAKE} )
+add_static_library(SimulDirectX12_MT ${SOURCES} ${HEADERS} ${CMAKE} )
 target_compile_definitions(SimulDirectX12_MT PRIVATE SIMUL_DIRECTX12_DLL=1)
 set_target_properties(SimulDirectX12_MT PROPERTIES FOLDER Static)
 target_include_directories(SimulDirectX12_MT PUBLIC "${CMAKE_SOURCE_DIR}/Platform/DirectX12")
@@ -20,7 +20,8 @@ LibraryDefaults(SimulDirectX12_MT)
 link_directories( ${CMAKE_SOURCE_DIR}/lib/x64/v141/${CMAKE_BUILD_TYPE})
 add_library(SimulDirectX12_MD SHARED ${SOURCES} ${HEADERS} ${CMAKE} )
 target_compile_definitions(SimulDirectX12_MD PRIVATE SIMUL_DYNAMIC_LINK=1 SIMUL_DIRECTX12_DLL=1)
-set_target_properties(SimulDirectX12_MD PROPERTIES FOLDER Dynamic)
+set_target_properties(SimulDirectX12_MD PROPERTIES FOLDER Dynamic
+							LINK_FLAGS "/DELAYLOAD:d3dcompiler_47.dll")
 target_include_directories(SimulDirectX12_MD PUBLIC "${CMAKE_SOURCE_DIR}/Platform/DirectX12")
 target_include_directories(SimulDirectX12_MD PRIVATE "${CMAKE_SOURCE_DIR}/External/DirectX/DirectXTex/DirectXTex" )
 LibraryDefaults(SimulDirectX12_MD)
