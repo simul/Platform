886003db37d04644a0558accc42faa62f5417da6
AndrewRichards-Code <andrew@simul.co>
2019-03-29 14:07:23 +0000

    Added check to ensure native OpenGL textures in Effect::SetUnorderedAccessView() are valid before binding them.

--- Platform/OpenGL/Effect.cpp
+++ Platform/OpenGL/Effect.cpp
@@ -271,11 +271,15 @@ void Effect::SetUnorderedAccessView(crossplatform::DeviceContext& deviceContext,
 void Effect::SetUnorderedAccessView(crossplatform::DeviceContext& deviceContext,const crossplatform::ShaderResource& name, crossplatform::Texture* tex, int index, int mip)
 {
     opengl::Texture* gTex = (opengl::Texture*)tex;
-    if (gTex)
-    {
-        GLuint imageView = gTex->AsOpenGLView(name.shaderResourceType, index, mip, true);
-        glBindImageTexture(name.slot, imageView, 0, GL_TRUE, 0, GL_READ_WRITE, RenderPlatform::ToGLFormat(tex->GetFormat()));
-    }
+	GLuint imageView;
+	if (gTex)
+	{
+		imageView = gTex->AsOpenGLView(name.shaderResourceType, index, mip, true);
+	}
+	if (glIsTexture(imageView))
+	{
+		glBindImageTexture(name.slot, imageView, 0, GL_TRUE, 0, GL_READ_WRITE, RenderPlatform::ToGLFormat(tex->GetFormat()));
+	}
     else
     {
         // Unbind it:
