12708fe233b9384fcc300c505a0f33f922e224d6
Roderick Kennedy <roderick.kennedy@gmail.com>
2020-02-11 21:47:50 +0000

    Remove old dependencies on SImulGeometry.

--- CrossPlatform/BaseRenderer.h
+++ CrossPlatform/BaseRenderer.h
@@ -58,18 +58,26 @@ namespace simul
 				,view_id(0)
 				,depthTextureStyle(DepthTextureStyle::PROJECTION)
 			{}
+			ViewStruct(const ViewStruct &vs):
+				initialized(false)
+				,view_id(vs.view_id)
+				,model(vs.model)
+				,view(vs.view)
+				,proj(vs.proj)
+				,depthTextureStyle(vs.depthTextureStyle)
+			{}
 			bool initialized;
-			int view_id;							///< An id unique to each rendered view, but persistent across frames.
-			math::Matrix4x4 model;
-			math::Matrix4x4 view;					///< The view matrix. If considered as row-major, position information is in the 4th row.
-			math::Matrix4x4 proj;					///< The projection matrix, row-major.
+			int view_id;			///< An id unique to each rendered view, but persistent across frames.
+			math::Matrix4x4 model;	///< The model matrix
+			math::Matrix4x4 view;	///< The view matrix. If considered as row-major, position information is in the 4th row.
+			math::Matrix4x4 proj;	///< The projection matrix, row-major.
+			// derived matrices
 			math::Matrix4x4 invViewProj;
 			math::Matrix4x4 invView;
 			math::Matrix4x4 viewProj;
-			//math::Matrix4x4 modelView;
-			//math::Matrix4x4 modelViewProj; 
+			// derived vectors.
 			vec3 cam_pos,view_dir,up;
-			simul::crossplatform::Frustum frustum;	///< THe viewing frustum, calculated from the proj matrix and stored for convenience using simul::crossplatform::GetFrustumFromProjectionMatrix.
+			simul::crossplatform::Frustum frustum;	///< The viewing frustum, calculated from the proj matrix and stored for convenience using simul::crossplatform::GetFrustumFromProjectionMatrix.
 			DepthTextureStyle depthTextureStyle;	///< How to interpret any depth texture passed from outside.
 			//! MUST be called whenever view or proj change.
 			void Init();
