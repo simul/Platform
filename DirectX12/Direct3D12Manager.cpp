eb00d2bc329c77a207e42f9961bcf930cd3784c4
Roderick Kennedy <roderick@simul.co.uk>
2020-04-26 14:33:52 +0100

    Don't leave D3d12 profiler queries mapped.

--- DirectX12/Direct3D12Manager.cpp
+++ DirectX12/Direct3D12Manager.cpp
@@ -330,7 +330,7 @@ void Direct3D12Manager::Shutdown()
 	IDXGIDebug1 *dxgiDebug;
 	if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebug))))
     {
-        dxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_FLAGS(DXGI_DEBUG_RLO_SUMMARY | DXGI_DEBUG_RLO_DETAIL| DXGI_DEBUG_RLO_IGNORE_INTERNAL));
+        dxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_FLAGS(DXGI_DEBUG_RLO_DETAIL| DXGI_DEBUG_RLO_IGNORE_INTERNAL));
     }
 	dxgiDebug->Release();
 #endif
