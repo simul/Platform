ca951234e33d3cea5a15ea8843adfb8a5bf44aa6
Simul Build <contact@simul.co>
2020-06-04 11:10:13 +0100

    Move fn to RenderPlatform.h due to compile order.

--- CrossPlatform/RenderPlatform.h
+++ CrossPlatform/RenderPlatform.h
@@ -445,6 +445,15 @@ namespace simul
 			}
 			return false;
 		}
+		template<class T, BufferUsageHint bufferUsageHint> void StructuredBuffer<T, bufferUsageHint>::RestoreDeviceObjects(RenderPlatform* p, int ct, bool computable, bool cpu_read, T* data, const char* n)
+		{
+			count = ct;
+			if (!platformStructuredBuffer)
+				platformStructuredBuffer = p->CreatePlatformStructuredBuffer();
+			else
+				platformStructuredBuffer->InvalidateDeviceObjects();
+			platformStructuredBuffer->RestoreDeviceObjects(p, count, sizeof(T), computable, cpu_read, data, n, bufferUsageHint);
+		}
 #endif
 	}
 }
