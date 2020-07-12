0ec367f694b9bda5237022396838deb51f32c7a8
Roderick Kennedy <roderick@simul.co.uk>
2020-07-12 09:31:11 +0100

    Cmake platform fixes.

--- OpenGL/FramebufferGL.cpp
+++ OpenGL/FramebufferGL.cpp
@@ -211,7 +211,7 @@ void FramebufferGL::Activate(crossplatform::DeviceContext& deviceContext)
 
     // Construct targets and viewport:
     targetsAndViewport.num              = 1;
-    targetsAndViewport.m_rt[0]          = (void*)mFBOId[fb];
+    targetsAndViewport.m_rt[0]          = (void*)(mFBOId[fb]);
     targetsAndViewport.m_dt             = 0;
     targetsAndViewport.viewport.x       = 0;
     targetsAndViewport.viewport.y       = 0;
