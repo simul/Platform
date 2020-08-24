b53a1c9d260ad72d84fb03512a3f1f55b5b105b2
Roderick Kennedy <roderick.kennedy@gmail.com>
2020-08-24 10:19:19 +0100

    Fix some compile errors in Linux.

--- CrossPlatform/Quaterniond.h
+++ CrossPlatform/Quaterniond.h
@@ -4,6 +4,7 @@
 #include "Platform/Math/Matrix.h"    
 #include "Platform/Shaders/SL/CppSl.sl"
 #include "Export.h"
+#include <algorithm>
 
 namespace simul
 {
@@ -273,7 +274,7 @@ namespace simul
 					z = z1;
 				}
 			}
-			template<typename U> void DefineSmall(T angle, const tvector3<U>& vv)
+			template<typename U> void DefineSmall(T ss, const tvector3<U>& vv)
 			{
 				ss *= T(0.5);
 				x = ss * T(vv.x);
