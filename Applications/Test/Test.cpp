ffc6ee46d3cde9033a2eccbda9e1beb1b7709418
Andrew Richards <andyjrichards93@gmail.com>
2021-01-22 15:10:16 +0000

    Set up CLEAR_COLOUR test in Test_MD/_MT

--- Applications/Test/Test.cpp
+++ Applications/Test/Test.cpp
@@ -92,21 +92,43 @@ int kOverrideWidth = 1440;
 int kOverrideHeight = 900;
 
 // from #include <Windowsx.h>
-#define GET_X_LPARAM(lp)                        ((int)(short)LOWORD(lp))
-#define GET_Y_LPARAM(lp)                        ((int)(short)HIWORD(lp))
-#define GET_WHEEL_DELTA_WPARAM(wParam)  ((short)HIWORD(wParam))
+#define GET_X_LPARAM(lp)				((int)(short)LOWORD(lp))
+#define GET_Y_LPARAM(lp)				((int)(short)HIWORD(lp))
+#define GET_WHEEL_DELTA_WPARAM(wParam)	((short)HIWORD(wParam))
+
+
+
+enum class TestType
+{
+	CLEAR_COLOUR,
+};
 
 class PlatformRenderer : public crossplatform::PlatformRendererInterface
 {
 public:
-	crossplatform::RenderPlatform* renderPlatform = nullptr;
 	crossplatform::RenderPlatformType renderPlatformType = crossplatform::RenderPlatformType::Unknown;
+	TestType testType;
 	bool debug = false;
+	const bool reverseDepth = true;
+	int framenumber = 0;
+
+	//Render Primitives
+	crossplatform::RenderPlatform*					renderPlatform	= nullptr;
+	crossplatform::Texture*							depthTexture	= nullptr;
+	crossplatform::HdrRenderer*						hdrRenderer		= nullptr;
+	crossplatform::BaseFramebuffer*					hdrFramebuffer	= nullptr;
+	crossplatform::Effect*							effect			= nullptr;
+	crossplatform::ConstantBuffer<SceneConstants>	sceneConstants;
+	crossplatform::ConstantBuffer<CameraConstants>	cameraConstants;
+
+	//Scene Objects
+	crossplatform::Camera							camera;
 
 public:
-	PlatformRenderer(const crossplatform::RenderPlatformType& type, bool use_debug)
-		:renderPlatformType(type), debug(use_debug)
+	PlatformRenderer(const crossplatform::RenderPlatformType& rpType, const TestType& tType, bool use_debug)
+		:renderPlatformType(rpType), testType(tType), debug(use_debug)
 	{
+		//Inital RenderPlatform and RenderDoc
 		switch (renderPlatformType)
 		{
 		case crossplatform::RenderPlatformType::D3D11:
@@ -138,94 +160,225 @@ public:
 
 		graphicsDeviceInterface->Initialize(debug, false, false);
 		if (debug)
-		{
 			crossplatform::RenderDocLoader::Load();
+
+		//RenderPlatforn Set up
+		renderPlatform->SetShaderBuildMode(simul::crossplatform::ShaderBuildMode::BUILD_IF_CHANGED);
+		renderPlatform->PushTexturePath("Textures");
+
+		switch (renderPlatformType)
+		{
+		case crossplatform::RenderPlatformType::D3D11:
+		{
+			renderPlatform->PushShaderPath("Platform/DirectX11/HLSL/");
+			renderPlatform->PushShaderPath("../../../../Platform/DirectX11/HLSL");
+			renderPlatform->PushShaderPath("../../Platform/DirectX11/HLSL");
+			break;
 		}
-	}
-	~PlatformRenderer()
-	{
-		delete renderPlatform;
+		default:
+		case crossplatform::RenderPlatformType::D3D12:
+		{
+			renderPlatform->PushShaderPath("Platform/DirectX12/HLSL/");
+			renderPlatform->PushShaderPath("../../../../Platform/DirectX12/HLSL");
+			renderPlatform->PushShaderPath("../../Platform/DirectX12/HLSL");
+			break;
+		}
+		case crossplatform::RenderPlatformType::Vulkan:
+		{
+			renderPlatform->PushShaderPath("Platform/Vulkan/GLSL/");
+			renderPlatform->PushShaderPath("../../../../Platform/Vulkan/GLSL");
+			renderPlatform->PushShaderPath("../../Platform/Vulan/GLSL");
+			break;
+		}
+		case crossplatform::RenderPlatformType::OpenGL:
+		{
+			renderPlatform->PushShaderPath("Platform/OpenGL/GLSL/");
+			renderPlatform->PushShaderPath("../../../../Platform/OpenGL/GLSL");
+			renderPlatform->PushShaderPath("../../Platform/OpenGL/GLSL");
+			break;
+		}
+		}
+
+		renderPlatform->PushShaderPath("Platform/Shaders/SFX/");
+		renderPlatform->PushShaderPath("../../../../Platform/CrossPlatform/SFX");
+		renderPlatform->PushShaderPath("../../Platform/CrossPlatform/SFX");
+		renderPlatform->PushShaderPath("../../../../Shaders/SFX/");
+		renderPlatform->PushShaderPath("../../../../Shaders/SL/");
+		renderPlatform->PushShaderPath("../../Shaders/SFX/");
+		renderPlatform->PushShaderPath("../../Shaders/SL/");
+
+		// Shader binaries: we want to use a shared common directory under Simul/Media. But if we're running from some other place, we'll just create a "shaderbin" directory.
+		std::string cmake_binary_dir = STRING_OF_MACRO(CMAKE_BINARY_DIR);
+		std::string cmake_source_dir = STRING_OF_MACRO(CMAKE_SOURCE_DIR);
+		if (cmake_binary_dir.length())
+		{
+			renderPlatform->PushShaderPath(((std::string(STRING_OF_MACRO(PLATFORM_SOURCE_DIR)) + "/") + renderPlatform->GetPathName() + "/HLSL").c_str());
+			renderPlatform->PushShaderPath(((std::string(STRING_OF_MACRO(PLATFORM_SOURCE_DIR)) + "/") + renderPlatform->GetPathName() + "/GLSL").c_str());
+			renderPlatform->PushShaderBinaryPath(((cmake_binary_dir + "/") + renderPlatform->GetPathName() + "/shaderbin").c_str());
+			std::string platform_build_path = ((cmake_binary_dir + "/Platform/") + renderPlatform->GetPathName());
+			renderPlatform->PushShaderBinaryPath((platform_build_path + "/shaderbin").c_str());
+			renderPlatform->PushTexturePath((cmake_source_dir + "/Resources/Textures").c_str());
+		}
+		renderPlatform->PushShaderBinaryPath((std::string("shaderbin/") + renderPlatform->GetPathName()).c_str());
+
+		//Set up HdrRenderer and Depth texture
+		depthTexture = renderPlatform->CreateTexture("Depth-Stencil"); //Calls new
+		hdrRenderer = new crossplatform::HdrRenderer();
+
+		//Set up BaseFramebuffer
+		hdrFramebuffer = renderPlatform->CreateFramebuffer(); //Calls new
+		hdrFramebuffer->SetFormat(crossplatform::RGBA_16_FLOAT);
+		hdrFramebuffer->SetDepthFormat(crossplatform::D_32_FLOAT);
+		hdrFramebuffer->SetAntialiasing(1);
+		hdrFramebuffer->DefaultClearColour = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+		hdrFramebuffer->DefaultClearDepth = reverseDepth ? 0.0f : 1.0f;
+		hdrFramebuffer->DefaultClearStencil = 0;
+
+		vec3 look = { 0.0f, 1.0f, 0.0f }, up = { 0.0f, 0.0f, 1.0f };
+		camera.LookInDirection(look, up);
+		camera.SetHorizontalFieldOfViewDegrees(90.0f);
+		camera.SetVerticalFieldOfViewDegrees(0.0f);// Automatic vertical fov - depends on window shape
+
+		crossplatform::CameraViewStruct vs;
+		vs.exposure = 1.0f;
+		vs.gamma = 0.44f;
+		vs.projection = reverseDepth ? crossplatform::DEPTH_REVERSE : crossplatform::FORWARD;
+		vs.nearZ = 0.1f;
+		vs.farZ = 300000.f;
+		vs.InfiniteFarPlane = true;
+		camera.SetCameraViewStruct(vs);
 	}
 
-	int AddView() override { return 0; };
+	~PlatformRenderer()
+	{
+		delete hdrFramebuffer;
+		delete hdrRenderer;
+		delete depthTexture;
+		delete renderPlatform;
+		graphicsDeviceInterface->Shutdown();
+	}
+
+	void OnCreateDevice()
+	{
+#ifdef SAMPLE_USE_D3D12
+		if (renderPlatformType == crossplatform::RenderPlatformType::D3D12)
+		{
+			// We will provide a command list so initialization of following resource can take place
+			((dx12::RenderPlatform*)renderPlatform)->SetImmediateContext((dx12::ImmediateContext*)dx12_deviceManager.GetImmediateContext());
+		}
+#endif
+		void* device = graphicsDeviceInterface->GetDevice();
+		renderPlatform->RestoreDeviceObjects(device);
+
+		hdrRenderer->RestoreDeviceObjects(renderPlatform);
+		hdrFramebuffer->RestoreDeviceObjects(renderPlatform);
+		effect = renderPlatform->CreateEffect();
+		effect->Load(renderPlatform, "solid");
+		sceneConstants.RestoreDeviceObjects(renderPlatform);
+		sceneConstants.LinkToEffect(effect, "SolidConstants");
+		cameraConstants.RestoreDeviceObjects(renderPlatform);
+
+#ifdef SAMPLE_USE_D3D12
+		if (renderPlatformType == crossplatform::RenderPlatformType::D3D12)
+		{
+			dx12_deviceManager.FlushImmediateCommandList();
+		}
+#endif
+	}
+
+	void OnLostDevice()
+	{
+		if (effect)
+		{
+			effect->InvalidateDeviceObjects();
+			delete effect;
+			effect = nullptr;
+		}
+		sceneConstants.InvalidateDeviceObjects();
+		cameraConstants.InvalidateDeviceObjects();
+		hdrRenderer->InvalidateDeviceObjects();
+		hdrFramebuffer->InvalidateDeviceObjects();
+		renderPlatform->InvalidateDeviceObjects();
+	}
+
+	void OnDestroyDevice()
+	{
+		OnLostDevice();
+	}
+
+	bool OnDeviceRemoved()
+	{
+		OnLostDevice();
+		return true;
+	}
+
+	int AddView() override 
+	{
+		static int last_view_id = 0;
+		return last_view_id++;
+	};
 	void RemoveView(int id) override {};
-	void Render(int view_id, void* context, void* colorBuffer, int w, int h, long long frame) override {};
 	void ResizeView(int view_id, int W, int H) override {};
+
+	void Render(int view_id, void* context, void* colorBuffer, int w, int h, long long frame) override
+	{
+		// Device context structure
+		simul::crossplatform::GraphicsDeviceContext	deviceContext;
+
+		// Store back buffer, depth buffer and viewport information
+		deviceContext.defaultTargetsAndViewport.num = 1;
+		deviceContext.defaultTargetsAndViewport.m_rt[0] = colorBuffer;
+		deviceContext.defaultTargetsAndViewport.rtFormats[0] = crossplatform::UNKNOWN; //To be later defined in the pipeline
+		deviceContext.defaultTargetsAndViewport.m_dt = nullptr;
+		deviceContext.defaultTargetsAndViewport.depthFormat = crossplatform::UNKNOWN;
+		deviceContext.defaultTargetsAndViewport.viewport = { 0, 0, w, h };
+		deviceContext.frame_number = framenumber;
+		deviceContext.platform_context = context;
+		deviceContext.renderPlatform = renderPlatform;
+		deviceContext.viewStruct.view_id = view_id;
+		deviceContext.viewStruct.depthTextureStyle = crossplatform::PROJECTION;
+		{
+			deviceContext.viewStruct.view = camera.MakeViewMatrix();
+			float aspect = (float)kOverrideWidth / (float)kOverrideHeight;
+			if (reverseDepth)
+			{
+				deviceContext.viewStruct.proj = camera.MakeDepthReversedProjectionMatrix(aspect);
+			}
+			else
+			{
+				deviceContext.viewStruct.proj = camera.MakeProjectionMatrix(aspect);
+			}
+			deviceContext.viewStruct.Init();
+		}
+
+		//Begin frame
+		renderPlatform->BeginFrame(deviceContext);
+
+		switch (testType)
+		{
+		default:
+		case TestType::CLEAR_COLOUR:
+		{
+			Test_ClearColour(deviceContext, w, h);
+			break;
+		}
+		}
+
+		hdrRenderer->Render(deviceContext, hdrFramebuffer->GetTexture(), 1.0f, 0.44f);
+
+		framenumber++;
+	}
+
+	void Test_ClearColour(crossplatform::GraphicsDeviceContext& deviceContext, int w, int h)
+	{
+		hdrFramebuffer->SetWidthAndHeight(w, h);
+		hdrFramebuffer->Activate(deviceContext);
+		hdrFramebuffer->Clear(deviceContext, 1.0f, 0.0f, 1.0f, 1.0f, reverseDepth ? 0.0f : 1.0f);
+		hdrFramebuffer->Deactivate(deviceContext);
+	}
 
 };
 PlatformRenderer* platformRenderer;
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	int wmId, wmEvent;
-	switch (message)
-	{
-	/*case WM_MOUSEWHEEL:
-		if (renderer)
-		{
-			int xPos = GET_X_LPARAM(lParam);
-			int yPos = GET_Y_LPARAM(lParam);
-			short zDelta = GET_WHEEL_DELTA_WPARAM(wParam);
-			renderer->OnMouse((wParam & MK_LBUTTON) != 0
-				, (wParam & MK_RBUTTON) != 0
-				, (wParam & MK_MBUTTON) != 0
-				, 0, xPos, yPos);
-		}
-		break;
-	case WM_MOUSEMOVE:
-		if (renderer)
-		{
-			int xPos = GET_X_LPARAM(lParam);
-			int yPos = GET_Y_LPARAM(lParam);
-			renderer->OnMouse((wParam & MK_LBUTTON) != 0
-				, (wParam & MK_RBUTTON) != 0
-				, (wParam & MK_MBUTTON) != 0
-				, 0, xPos, yPos);
-		}
-		break;
-	case WM_KEYDOWN:
-		if (renderer)
-			renderer->OnKeyboard((unsigned)wParam, true);
-		break;
-	case WM_KEYUP:
-		if (renderer)
-			renderer->OnKeyboard((unsigned)wParam, false);
-		break;
-	case WM_COMMAND:
-		wmId = LOWORD(wParam);
-		wmEvent = HIWORD(wParam);
-		// Parse the menu selections:
-		//switch (wmId)
-		return DefWindowProc(hWnd, message, wParam, lParam);
-		break;
-	case WM_SIZE:
-		if (renderer)
-		{
-			INT Width = LOWORD(lParam);
-			INT Height = HIWORD(lParam);
-			if (Width > 8192 || Height > 8192 || Width < 0 || Height < 0)
-				break;
-			displaySurfaceManager.ResizeSwapChain(hWnd);
-		}
-		break;
-	case WM_PAINT:
-		if (renderer)
-		{
-			double fTime = 0.0;
-			float time_step = 0.01f;
-			renderer->OnFrameMove(fTime, time_step);
-			displaySurfaceManager.Render(hWnd);
-			displaySurfaceManager.EndFrame();
-		}
-		break;*/
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return 0;
-}
 
 simul::crossplatform::RenderPlatformType GetRenderPlatformTypeFromCmdLnArgs(wchar_t** szArgList, int argCount)
 {
@@ -261,6 +414,99 @@ simul::crossplatform::RenderPlatformType GetRenderPlatformTypeFromCmdLnArgs(wcha
 	}
 }
 
+TestType GetTestTypeFromCmdLnArgs(wchar_t** szArgList, int argCount)
+{
+	bool testFound = false;
+	const size_t tagSize = std::string("-X:").size();
+	for (int i = 0; i < argCount; i++)
+	{
+		std::wstring arg = szArgList[i];
+		if (arg.find(L"-t:") != std::string::npos || arg.find(L"-T:") != std::string::npos)
+		{
+			std::wstring value = arg.erase(0, tagSize);
+			return static_cast<TestType>(_wtoi(value.c_str()));
+		}
+	}
+	if(!testFound)
+		{
+			SIMUL_COUT << "Unknown TestType. This should be a specified on the command line arguments\n";
+			SIMUL_COUT << "Defaulting to TestType::CLEAR_COLOUR.\n";
+			return TestType::CLEAR_COLOUR;
+		}
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	switch (message)
+	{
+		/*case WM_MOUSEWHEEL:
+			if (renderer)
+			{
+				int xPos = GET_X_LPARAM(lParam);
+				int yPos = GET_Y_LPARAM(lParam);
+				short zDelta = GET_WHEEL_DELTA_WPARAM(wParam);
+				renderer->OnMouse((wParam & MK_LBUTTON) != 0
+					, (wParam & MK_RBUTTON) != 0
+					, (wParam & MK_MBUTTON) != 0
+					, 0, xPos, yPos);
+			}
+			break;
+		case WM_MOUSEMOVE:
+			if (renderer)
+			{
+				int xPos = GET_X_LPARAM(lParam);
+				int yPos = GET_Y_LPARAM(lParam);
+				renderer->OnMouse((wParam & MK_LBUTTON) != 0
+					, (wParam & MK_RBUTTON) != 0
+					, (wParam & MK_MBUTTON) != 0
+					, 0, xPos, yPos);
+			}
+			break;
+		case WM_KEYDOWN:
+			if (renderer)
+				renderer->OnKeyboard((unsigned)wParam, true);
+			break;
+		case WM_KEYUP:
+			if (renderer)
+				renderer->OnKeyboard((unsigned)wParam, false);
+			break;
+		case WM_COMMAND:
+			wmId = LOWORD(wParam);
+			wmEvent = HIWORD(wParam);
+			// Parse the menu selections:
+			//switch (wmId)
+			return DefWindowProc(hWnd, message, wParam, lParam);
+			break;
+		case WM_SIZE:
+			if (renderer)
+			{
+				INT Width = LOWORD(lParam);
+				INT Height = HIWORD(lParam);
+				if (Width > 8192 || Height > 8192 || Width < 0 || Height < 0)
+					break;
+				displaySurfaceManager.ResizeSwapChain(hWnd);
+			}
+			break;*/
+	case WM_PAINT:
+		if (platformRenderer)
+		{
+			/*double fTime = 0.0;
+			float time_step = 0.01f;
+			renderer->OnFrameMove(fTime, time_step);*/
+			displaySurfaceManager.Render(hWnd);
+			displaySurfaceManager.EndFrame();
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
 int APIENTRY WinMain(HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR    lpCmdLine,
@@ -271,6 +517,8 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 	szArgList = CommandLineToArgvW(GetCommandLineW(), &argCount);
 
 	simul::crossplatform::RenderPlatformType x64_API = GetRenderPlatformTypeFromCmdLnArgs(szArgList, argCount);
+
+	TestType test = GetTestTypeFromCmdLnArgs(szArgList, argCount);
 
 #ifdef _MSC_VER
 	// The following disables error dialogues in the case of a crash, this is so automated testing will not hang. See http://blogs.msdn.com/b/oldnewthing/archive/2004/07/27/198410.aspx
@@ -312,13 +560,10 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 		UpdateWindow(hWnd);
 	}
 
-	platformRenderer = new PlatformRenderer(x64_API, commandLineParams("debug"));
+	platformRenderer = new PlatformRenderer(x64_API, test, commandLineParams("debug"));
+	platformRenderer->OnCreateDevice();
 	displaySurfaceManager.Initialize(platformRenderer->renderPlatform);
-
-	// Create an instance of our simple renderer class defined above:
-	//platformRenderer->OnCreateDevice(graphicsDeviceInterface->GetDevice());
-	//displaySurfaceManager.AddWindow(hWnd);
-	//displaySurfaceManager.SetRenderer(hWnd, platformRenderer, -1);
+	displaySurfaceManager.SetRenderer(hWnd, platformRenderer, -1);
 
 	// Main message loop:
 	MSG msg;
@@ -330,8 +575,11 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 		//if (commandLineParams.quitafterframe > 0 && renderer->framenumber >= commandLineParams.quitafterframe)
 		//	break;
 	}
+	displaySurfaceManager.RemoveWindow(hWnd);
+	displaySurfaceManager.Shutdown();
+	platformRenderer->OnDeviceRemoved();
 	delete platformRenderer;
-	return (int)0;// msg.wParam;
+	return 0;
 }
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
