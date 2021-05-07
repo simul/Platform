4e2e6ae3dba1f8cf9c15324acd0aeb76b73079d7
Andrew Richards <andyjrichards93@gmail.com>
2021-05-07 13:40:50 +0100

    Removed redundant GFX.hlsl. RootSignature is generated in code.

--- DirectX12/Sfx/GFX.hlsl
+++
@@ -1,2 +0,0 @@
-#define GFXRS "RootFlags(ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT), DescriptorTable(CBV(b0, numDescriptors = 14),SRV(t0, numDescriptors = 24),UAV(u0, numDescriptors = 16)),DescriptorTable(Sampler(s0, numDescriptors = 16))" 
-
