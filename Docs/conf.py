11618c245a0bd44a2ccbd19d178a9ab276be7c13
Roderick Kennedy <roderick.kennedy@gmail.com>
2021-06-20 21:43:07 +0100

    conf.py modified for specific readthedocs config.

--- Docs/conf.py
+++ Docs/conf.py
@@ -13,7 +13,29 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import subprocess, os
 
+def configureDoxyfile(input_dir, output_dir):
+    with open('Doxyfile.in', 'r') as file :
+        filedata = file.read()
+
+    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+
+    with open('Doxyfile', 'w') as file:
+        file.write(filedata)
+
+# Check if we're running on Read the Docs' servers
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+breathe_projects = {}
+
+if read_the_docs_build:
+    input_dir = '../'
+    output_dir = 'build'
+    configureDoxyfile(input_dir, output_dir)
+    subprocess.call('doxygen', shell=True)
+    breathe_projects['Platform'] = output_dir + '/xml'
 
 # -- Project information -----------------------------------------------------
 
@@ -53,10 +75,4 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 breathe_default_project = "Platform"
-import os
-working_dir=os.getcwd()
-print(working_dir)
-if working_dir.split('/')[-1]=="Docs":
-	print("going up one")
-	os.chdir('..')
 master_doc = 'platform'
