6b9599d2f1abdfeb6d2769334c8e95984521ad28
Andrew Richards <andyjrichards93@gmail.com>
2021-10-13 10:29:16 +0100

    Changed std::this_thread::yield() for std::this_thread::sleep_for() in RenderPlatform::AsyncResetCommandAllocator().

--- DirectX12/RenderPlatform.cpp
+++ DirectX12/RenderPlatform.cpp
@@ -1400,7 +1400,7 @@ void RenderPlatform::AsyncResetCommandAllocator()
 			mMutexReleaseAllocators.unlock();
 		}
 
-		std::this_thread::yield();
+		std::this_thread::sleep_for(std::chrono::seconds(1));
 	}
 
 	//Clean up after ~RenderPlaform();
