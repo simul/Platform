c76de9e0c523bdf218afb85f139b4ecae5890af4
Roderick Kennedy <roderick@simul.co.uk>
2022-03-28 17:02:38 +0100

    #ifdef SIMUL_DEBUG_BARRIERS should be #if SIMUL_DEBUG_BARRIERS in Texture.cpp.

--- DirectX12/Texture.cpp
+++ DirectX12/Texture.cpp
@@ -2022,7 +2022,7 @@ void Texture::SplitLayouts()
 
 void Texture::AssumeLayout(D3D12_RESOURCE_STATES state)
 {
-#ifdef SIMUL_DEBUG_BARRIERS
+#if SIMUL_DEBUG_BARRIERS
 	SIMUL_COUT<<name.c_str()<<" 0x"<<std::hex<<(unsigned long long)mTextureDefault<<" assumed as layout "<<dx12::RenderPlatform::D3D12ResourceStateToString(state).c_str()<<std::endl;
 #endif
     int numLayers       = (int)mSubResourcesStates.size();
