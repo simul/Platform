a294b44bdd54c4c2aa5d9c18d77f57ef0d4ddc24
Roderick Kennedy <roderick.kennedy@gmail.com>
2023-04-03 13:24:00 +0100

    Doxygen debug.

--- Docs/conf.py
+++ Docs/conf.py
@@ -16,27 +16,28 @@
 import subprocess, os
 
 def configureDoxyfile(input_dir, output_dir):
-    with open('Doxyfile.in', 'r') as file :
-        filedata = file.read()
+	with open('Doxyfile.in', 'r') as file :
+		filedata = file.read()
 
-    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
-    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
-    filedata = filedata.replace('@DOXYGEN_SEARCH_PATH@', '..')
+	filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+	filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+	filedata = filedata.replace('@DOXYGEN_SEARCH_PATH@', '..')
 	
-    with open('Doxyfile', 'w') as file:
-        file.write(filedata)
+	with open('Doxyfile', 'w') as file:
+		file.write(filedata)
 
 # Check if we're running on Read the Docs' servers
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
-
+print("read_the_docs_build: "+read_the_docs_build)
 breathe_projects = {}
 
 if read_the_docs_build:
-    input_dir = './'
-    output_dir = '../build'
-    configureDoxyfile(input_dir, output_dir)
-    subprocess.call('doxygen', shell=True)
-    breathe_projects['Platform'] = output_dir + '/xml'
+	input_dir = './'
+	output_dir = '../build'
+	configureDoxyfile(input_dir, output_dir)
+	print('Executing doxygen at '+os.getcwd())
+	subprocess.call('doxygen', shell=True)
+	breathe_projects['Platform'] = output_dir + '/xml'
 
 # -- Project information -----------------------------------------------------
 
@@ -52,7 +53,7 @@ author = 'Roderick Kennedy'
 # ones.
 extensions = [ "breathe","myst_parser",'sphinx.ext.autosectionlabel']
 source_suffix = {
-    '.rst': 'restructuredtext',
+	'.rst': 'restructuredtext',
 }
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
