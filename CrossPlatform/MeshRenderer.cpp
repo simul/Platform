0555b9fe4848253173b03c5fe093fe9c4317a754
Andrew Richards <andyjrichards93@gmail.com>
2024-04-18 15:09:13 +0100

    Fixed VS_Solid() matrix multiplication.

--- CrossPlatform/MeshRenderer.cpp
+++ CrossPlatform/MeshRenderer.cpp
@@ -41,7 +41,6 @@ void MeshRenderer::InvalidateDeviceObjects()
 void MeshRenderer::DrawSubMesh(GraphicsDeviceContext& deviceContext, Mesh* mesh, int index)
 {
 	cameraConstants.world = deviceContext.viewStruct.model;
-	cameraConstants.worldViewProj = deviceContext.viewStruct.viewProj;
 	cameraConstants.view = deviceContext.viewStruct.view;
 	cameraConstants.proj = deviceContext.viewStruct.proj;
 	cameraConstants.viewPosition = deviceContext.viewStruct.cam_pos;
